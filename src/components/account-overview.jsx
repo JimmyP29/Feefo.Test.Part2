4548bb433c4dfaa6291ccf302472a00f7132c95c
CATERNET\james.privett <james.privett@zupa.co.uk>
2021-02-06 15:36:15 +0000

    Created Grid and implemented for main layout

--- src/components/account-overview.jsx
+++ src/components/account-overview.jsx
@@ -1,14 +1,40 @@
 import React from 'react';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faHardHat } from '@fortawesome/free-solid-svg-icons';
-//import styled from 'styled-components';
+// import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+// import { faHardHat } from '@fortawesome/free-solid-svg-icons';
+import styled from 'styled-components';
 import { string, number, shape } from 'prop-types';
 
 import ContactDetails from './header/contact-details';
 import SalesOverview from './body/sales-overview';
 
+// import MainWrapper from './styles/account-overview';
+// import AccountHeader from './styles/account-header';
+import { Grid, Row, Col } from './styles/grid';
 
-import './account-overview.css';
+const MainWrapper = styled.section`
+    background-color: lightblue;
+    /* display: flex; */
+    /* flex-direction: column; */
+   
+    /* justify-content: center; */
+    font-family: 'Roboto', sans-serif;
+    /* text-align: center; */
+`;
+
+// const AccountHeader = styled.div`
+//     color: #282c34;
+//    // min-height: 100vh;
+//     display: flex;
+//     flex-direction: column;
+//     //align-items: center;
+//     justify-content: flex-start;
+//     //font-size: calc(10px + 2vmin);
+// `;
+
+const Title = styled.h1`
+  color: #282c34;
+`;
+
 
 export const AccountOverview = ({ data }) => {
   console.log(data);
@@ -17,17 +43,30 @@ export const AccountOverview = ({ data }) => {
     salesOverview,
   } = data;
   return (
-    <div className="AccountOverview">
-      <FontAwesomeIcon icon={faHardHat} />
-      Build your components here
-      <ContactDetails
-        name={name}
-        email={email}
-      />
-      <SalesOverview
-        salesOverview={salesOverview}
-      />
-    </div>
+    <>
+      <MainWrapper>
+        <Grid>
+          <Row>
+            <Col size={1}>
+              <Title>Account Overview</Title>
+            </Col>
+            <Col size={2}>
+              <ContactDetails
+                name={name}
+                email={email}
+              />
+            </Col>
+          </Row>
+          <Row>
+            <Col size={1}>
+              <SalesOverview
+                salesOverview={salesOverview}
+              />
+            </Col>
+          </Row>
+        </Grid>
+      </MainWrapper>
+    </>
   )
 }
 
