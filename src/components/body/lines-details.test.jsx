67000975c5885a4d97598aca0c94a34ca5ec0abf
CATERNET\james.privett <james.privett@zupa.co.uk>
2021-02-07 16:40:17 +0000

    Lines details tests

---
+++ src/components/body/lines-details.test.jsx
@@ -0,0 +1,30 @@
+import { render, screen, waitFor } from '@testing-library/react';
+import LinesDetails from './lines-details';
+import { percentageRenderData } from '../testData';
+
+describe('Given the LinesDetails component', () => {
+    describe.each([
+        percentageRenderData.happy1,
+        percentageRenderData.happy2,
+        percentageRenderData.happy3,
+    ])('And happy path data passed in is valid', (testCase) => {
+        test('Then upload details are rendered', async () => {
+            render(<LinesDetails linePercentage={testCase.input} />);
+            await waitFor(() => screen.getByText(`${testCase.input}%`))
+
+            expect(screen.getByText(`${testCase.input}%`)).toHaveTextContent(testCase.output);
+        });
+    });
+
+    describe.each([
+        percentageRenderData.sad1,
+        percentageRenderData.sad2,
+    ])('And sad path data passed in is valid', (testCase) => {
+        test('Then upload details are rendered', async () => {
+            render(<LinesDetails linePercentage={testCase.input} />);
+            await waitFor(() => screen.getByText(testCase.input))
+
+            expect(screen.getByText(testCase.input)).toHaveTextContent(testCase.output);
+        });
+    });
+});
