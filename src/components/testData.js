a454d5efbc79be5c6dbd90a70a6037783d5f8a9e
CATERNET\james.privett <james.privett@zupa.co.uk>
2021-02-07 15:34:40 +0000

    testData file

---
+++ src/components/testData.js
@@ -0,0 +1,13 @@
+export const initialSetup = {
+    supportContact: {
+        name: 'James Privett',
+        email: 'james.privett@test.com'
+    },
+    salesOverview: {
+        uploads: 8,
+        successfulUploads: 3,
+        linesAttempted: 20,
+        linesSaved: 4,
+        lastUploadDate: 1605001226079,
+    }
+};
